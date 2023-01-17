5dfd7d26c19909b4c14de807d9a5bd72e2b7e218
Bradley Thomas <bradley.thomas60@googlemail.com>
2023-01-17 18:59:47 +0000

    Added Main menu, Stopped program from exiting when calculation printed, Added calculations class, added menu conversion class

---
+++ electricity-calculator/src/Formulas/ElectricalCalculator.cs
@@ -0,0 +1,32 @@
+﻿namespace electricity_calculator.src
+{
+  public class ElectricalCalculator
+  {
+    public float CalculateVoltage(float current, float resistance)
+    {
+      Electricity electricity = new Electricity();
+      electricity.Current = current;
+      electricity.Resistance = resistance;
+
+      return electricity.Current * electricity.Resistance;
+    }
+
+    public float CalculateCurrent(float current, float resistance)
+    {
+      Electricity electricity = new Electricity();
+      electricity.Current = current;
+      electricity.Resistance = resistance;
+
+      return electricity.Current / electricity.Resistance;
+    }
+
+    public float CalculateWattage(float voltage, float current)
+    {
+      Electricity electricity = new Electricity();
+      electricity.Voltage = voltage;
+      electricity.Current = current;
+
+      return electricity.Voltage * electricity.Current;
+    }
+  }
+}
