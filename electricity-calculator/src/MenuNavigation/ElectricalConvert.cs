873dfaf5af5d7216dae98051bc87cf0382eb5d3b
Bradley Thomas <bradley.thomas60@googlemail.com>
2023-01-17 18:59:47 +0000

    Added Main menu, Stopped program from exiting when calculation printed, Added calculations class, added menu conversion class

---
+++ electricity-calculator/src/MenuNavigation/ElectricalConvert.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace electricity_calculator.src.MenuNavigation
+{
+  public class ElectricalConvert
+  {
+    public float ConvertVoltage(ElectricalCalculator calculate)
+    {
+      Console.WriteLine("Input Current (A)");
+      string userInput = Console.ReadLine();
+      float userCurrent = float.Parse(userInput);
+
+      Console.WriteLine("Input Resistance (Ohms)");
+      userInput = Console.ReadLine();
+      float userResistance = float.Parse(userInput);
+
+      return calculate.CalculateVoltage(userCurrent, userResistance);
+    }
+
+    public float ConvertCurrent(ElectricalCalculator calculate)
+    {
+      Console.WriteLine("Input Voltage (V)");
+      string userInput = Console.ReadLine();
+      float userCurrent = float.Parse(userInput);
+
+      Console.WriteLine("Input Resistance (Ohms)");
+      userInput = Console.ReadLine();
+      float userResistance = float.Parse(userInput);
+
+      return calculate.CalculateCurrent(userCurrent, userResistance);
+    }
+
+    public float ConvertWattage(ElectricalCalculator calculate)
+    {
+      Console.WriteLine("Input Voltage (V)");
+      string userInput = Console.ReadLine();
+      float userCurrent = float.Parse(userInput);
+
+      Console.WriteLine("Input Current (A)");
+      userInput = Console.ReadLine();
+      float userResistance = float.Parse(userInput);
+
+      return calculate.CalculateWattage(userCurrent, userResistance);
+    }
+  }
+}
--- electricity-calculator/src/Program.cs
+++ electricity-calculator/src/Program.cs
@@ -1,91 +1,46 @@
-﻿namespace electricity_calculator.src
+﻿using electricity_calculator.src.MenuNavigation;
+
+namespace electricity_calculator.src
 {
   public class Program
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("What do you want to convert?");
-      string userInput = Console.ReadLine();
+      ElectricalConvert convert = new ElectricalConvert();
+      ElectricalCalculator calculate = new ElectricalCalculator();
+      bool running = true;
 
-      if (userInput == "1")
+      while (running = true)
       {
-        ConvertVoltage();
+        Console.Clear();
+        Console.WriteLine("1) Calculate Voltage");
+        Console.WriteLine("2) Calculate Current");
+        Console.WriteLine("3) Calculate Wattage");
+        Console.WriteLine("\nE to Exit");
+        Console.WriteLine("\nWhat do you want to convert?");
+        string userInput = Console.ReadLine();
+
+        if (userInput == "1")
+        {
+          Console.WriteLine($"Calculated Voltage: {convert.ConvertVoltage(calculate)} v");
+        }
+        else if (userInput == "2")
+        {
+          Console.WriteLine($"Calculated Current: {convert.ConvertCurrent(calculate)} A");
+        }
+        else if (userInput == "3")
+        {
+          Console.WriteLine($"Calculated Wattage: {convert.ConvertWattage(calculate)} w");
+        }
+        else if ((userInput == "e") || (userInput == "E"))
+        {
+          running = false;
+          Environment.Exit(0);
+        }
+
+        Console.WriteLine("Hit ENTER to restart");
+        Console.ReadLine();
       }
-      else if (userInput == "2")
-      {
-        ConvertCurrent();
-      }
-      else if (userInput == "3")
-      {
-        ConvertWattage();
-      }
-    }
-    
-    private static void ConvertVoltage()
-    {
-      Console.WriteLine("Input Current (A)");
-      string userInput = Console.ReadLine();
-      float userCurrent = Int64.Parse(userInput);
-
-      Console.WriteLine("Input Resistance (Ohms)");
-      userInput = Console.ReadLine();
-      float userResistance = Int64.Parse(userInput);
-
-      Console.WriteLine(CalculateVolts(userCurrent, userResistance));
-    }
-
-    private static void ConvertCurrent()
-    {
-      Console.WriteLine("Input Voltage (V)");
-      string userInput = Console.ReadLine();
-      float userCurrent = Int64.Parse(userInput);
-
-      Console.WriteLine("Input Resistance (Ohms)");
-      userInput = Console.ReadLine();
-      float userResistance = Int64.Parse(userInput);
-
-      Console.WriteLine(CalculateCurrent(userCurrent, userResistance));
-    }
-
-    private static void ConvertWattage()
-    {
-      Console.WriteLine("Input Voltage (V)");
-      string userInput = Console.ReadLine();
-      float userCurrent = Int64.Parse(userInput);
-
-      Console.WriteLine("Input Current (A)");
-      userInput = Console.ReadLine();
-      float userResistance = Int64.Parse(userInput);
-
-      Console.WriteLine(CalculateWattage(userCurrent, userResistance));
-    }
-
-    private static float CalculateWattage(float voltage, float current)
-    {
-      Electricity electricity = new Electricity();
-      electricity.Current = current;
-      electricity.Voltage = voltage;
-
-      return electricity.Current * electricity.Resistance;
-    }
-
-    private static float CalculateCurrent(float current, float resistance)
-    {
-      Electricity electricity = new Electricity();
-      electricity.Current = current;
-      electricity.Resistance = resistance;
-
-      return electricity.Current / electricity.Resistance;
-      
-    }
-
-    public static float CalculateVolts(float current, float resistance)
-    {
-      Electricity electricity = new Electricity();
-      electricity.Current = current;
-      electricity.Resistance = resistance;
-      
-      return electricity.Current * electricity.Resistance;
     }
   }
 }
